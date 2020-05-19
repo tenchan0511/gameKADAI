3df2f079040f2efd055971e8d4055d68969a9885
tenchan0511 <acchan0511@ezweb.ne.jp>
2020-05-19 15:14:14 +0900

    敵3種類の描画登録と基盤の制作

--- Project1/enemy.cpp
+++ Project1/enemy.cpp
@@ -5,13 +5,62 @@
 #include "stage.h"
 
 // ïœêî
-CHARACTER enemy1;
-int enemy1Image[4][3];
+CHARACTER enemy[ENEMY_MAX];
+CHARACTER enemyindividual[ENEMY_TYPE_MAX];
+int enemyImage[ENEMY_TYPE_MAX][12];
 int moveCnt;
 
 void EnemySystemInit(void)
 {
-	
+	// enemy1
+	enemyindividual[ENEMY_TYPE_NORMAL].size.x = ENEMY_SIZE_X;
+	enemyindividual[ENEMY_TYPE_NORMAL].size.y = ENEMY_SIZE_Y;
+	enemyindividual[ENEMY_TYPE_NORMAL].moveSpeed = 2;
+	LoadDivGraph(
+		"image/enemy01.png",
+		12,
+		3,
+		4,
+		ENEMY_SIZE_X,
+		ENEMY_SIZE_Y,
+		&enemyImage[ENEMY_TYPE_NORMAL][0]
+	);
+
+	// enemy2
+	enemyindividual[ENEMY_TYPE_FAST].size.x = ENEMY_SIZE_X;
+	enemyindividual[ENEMY_TYPE_FAST].size.y = ENEMY_SIZE_Y;
+	enemyindividual[ENEMY_TYPE_FAST].moveSpeed = 5;
+	LoadDivGraph(
+		"image/enemy03.png",
+		12,
+		3,
+		4,
+		ENEMY_SIZE_X,
+		ENEMY_SIZE_Y,
+		&enemyImage[ENEMY_TYPE_FAST][0]
+	);
+
+	// enemy3
+	enemyindividual[ENEMY_TYPE_SEARCH].size.x = ENEMY_SIZE_X;
+	enemyindividual[ENEMY_TYPE_SEARCH].size.y = ENEMY_SIZE_Y;
+	enemyindividual[ENEMY_TYPE_SEARCH].moveSpeed = 3;
+	LoadDivGraph(
+		"image/enemy02.png",
+		12,
+		3,
+		4,
+		ENEMY_SIZE_X,
+		ENEMY_SIZE_Y,
+		&enemyImage[ENEMY_TYPE_SEARCH][0]
+	);
+
+	for (int y = 0; y < ENEMY_TYPE_MAX; y++)
+	{
+		enemyindividual[y].moveDir = DIR_DOWN;
+		enemyindividual[y].offsetSize.x = enemyindividual[y].size.x / 2;
+		enemyindividual[y].offsetSize.y = enemyindividual[y].size.y / 2;
+
+	}
 }
 
 bool EnemyGameInit(void)
@@ -30,5 +79,9 @@ void EnemyControl(XY playerPos)
 
 void EnemyDrawInit(void)
 {
-	DrawGraph(enemy1.pos.x, enemy1.pos.y, enemy1Image[1][0], true);
+	DrawGraph(
+		enemyindividual[ENEMY_TYPE_NORMAL].pos.x, 
+		enemyindividual[ENEMY_TYPE_NORMAL].pos.y,
+		enemyImage[ENEMY_TYPE_NORMAL][0],
+		true);
 }
--- Project1/enemy.h
+++ Project1/enemy.h
@@ -5,13 +5,13 @@
 #define ENEMY_SIZE_X 32	// 敵キャラ横サイズ
 #define ENEMY_SIZE_Y 48 // 敵キャラ縦サイズ
 
-//enum ENEMY_TYPE
-//{
-//	ENEMY_TYPE_NORMAL,
-//	ENEMY_TYPE_FAST,
-//	ENEMY_TYPE_SEARCH,
-//	ENEMY_TYPE_MAX
-//};
+enum ENEMY_TYPE
+{
+	ENEMY_TYPE_NORMAL,
+	ENEMY_TYPE_FAST,
+	ENEMY_TYPE_SEARCH,
+	ENEMY_TYPE_MAX
+};
 
 // プロトタイプ宣言
 void EnemySystemInit(void);
